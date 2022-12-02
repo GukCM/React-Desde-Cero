78bb271a3b1f04e453c274f06160d0dc40029f2f
GukCM <gusavocod@gmail.com>
2022-12-01 18:01:50 -0600

    Intro javascript

---
+++ 02-intro-javascript/src/bases/03-Metodos-Academy.js
@@ -0,0 +1,31 @@
+// let nombre = 'Carlos';
+// let apellido = 'Damian';
+// console.log(nombre.charAt(5)); Regresa según el índice
+// console.log(nombre.concat(` ${apellido}`)); Template string
+// console.log(nombre.includes('Ca')); Case sensitive, regresa true porque nombre contiene "Ca" exactamente igual
+// toLowerCase() para poner en minusculas
+// console.log(nombre.toUpperCase()); Regresa todo en mayusculas
+// console.log(nombre.indexOf('r')); Regresa el indice si existe, si no existe, regresa un "-1"
+//Si indexof me regresa -1 -> agrega el elemento => significa que no existe
+// console.log(nombre.length); Devuelve la longitud 
+// console.log(nombre.slice(2,5)); Recibe un index de inicio y uno de final
+// console.log(nombre.split('r')); Corta el objeto por el string que le indiques y devuelve un array
+// console.log(apellido.trim());Elimina los espacios
+// console.log(apellido.split(" ").join("").trim()); quitar espacios entre una palabra
+// console.log(nombre.padEnd(30,'.')); Recibe un max length y un string que agregará hasta que se cumpla el length
+// console.log(nombre.lastIndexOf('o')); Retorna el index donde se vió por ultima vez el string
+// Una expresion regular ayuda a dar patrones para cumplir un objetivo, por ejemplo si queremos que una oracion cumpla un criterio
+// por ejemplo, un minimo de 6 caracteres y que tenga un arroba
+
+let array1 = ['Carlos'];
+let array2 = ['Damian'];
+// console.log(array1.concat(array2)); Concatena un array con otro
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+// const nuevoArray = numbers.filter((item) => item > 5); Filtra el arreglo según la condición dada
+// console.log(nuevoArray);
+
+
+
+
+
+
