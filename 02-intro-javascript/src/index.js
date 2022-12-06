26ed32cbb2a9a6db2e7ae4d7954db0ca5e79d33c
GukCM <gusavocod@gmail.com>
2022-12-06 15:38:05 -0600

    Filter & find, Import & Export

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,6 +1,26 @@
+// import { heroes } from './data/heroes'
+// import { heroes } from './data/heroes'
+import { heroes } from "./data/heroes";
 
+console.log(heroes);
 
+// const getHeroeById = (id) =>{
+//     return heroes.find((heroes) =>{
+//         if(heroes.id === id){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     } );
+// }
 
+// const getHeroeById = (id) =>{
+//     return heroes.find(heroe => heroe.id === id); 
+// }
 
+const getHeroeById = (id) => heroes.find(heroe => heroe.id === id); 
+console.log(getHeroeById(2));
 
+const getHeroesByOwner = (owner) => heroes.filter(heroe => heroe.owner === owner);
 
+console.log(getHeroesByOwner('DC'));
