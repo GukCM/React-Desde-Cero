ac583091505effce2d794cd77321bb697d66e107
GukCM <gusavocod@gmail.com>
2022-12-18 23:38:28 -0600

    Primer componente, maneras de exportarlo

--- 03-counter-app-vite/src/HelloWorldApp.jsx
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -2,5 +2,7 @@
 //Functional components, componentes basados en funciones
 export function App(){
 
-    return (<h1>Hola mundo!!!</h1>);
+    return (<h1>Hola mundo!</h1>);
 }
+
+// export const App = () =>  (<h1>Hola Mundo!!</h1>);
