a34dd3e1492068547bcaea429d298b1e48842f4d
GukCM <gusavocod@gmail.com>
2022-12-20 22:39:41 -0600

    Modificacion

--- 03-counter-app-vite/src/MyName.jsx
+++ 03-counter-app-vite/src/MyName.jsx
@@ -1,3 +1,9 @@
 export function MyName(){
-    return (<h1>Soy Gustavo</h1>)
+    return (<h1>Soy Gustavo!</h1>)
 }
+
+// import { useState } from "react"
+// export const MyName = () =>{
+//     const [count] = useState(0);
+//     return <button onClick={alert("Click me")}>{count}</button>
+// }
